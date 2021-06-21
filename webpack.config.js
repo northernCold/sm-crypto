9c50bd139e906bedcf860dcee353c35e48e9b971
黄晓东 <1357805398@qq.com>
2021-06-21 16:01:10 +0800

    chore: modify npm script and webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,43 @@
 const path = require('path');
-const webpack = require('webpack');
 const es3ifyWebpackPlugin = require('es3ify-webpack-plugin');
 const TerserPlugin = require("terser-webpack-plugin");
+const libraryName = require('./package.json').name
+const { CIPHER } = process.env;
+
+const entryGenerator = cipher => {
+  if (cipher) {
+    return { [cipher]: `./src/${cipher}/index.js`}
+  }
+  return './src/index.js';
+}
+
+const outputGenerator = cipher => {
+  if (cipher) {
+    return {
+      path: path.resolve(__dirname, 'dist'),
+      filename: '[name].js',
+      library: {
+        name: '[name]',
+        type: 'umd',
+        umdNamedDefine: true
+      }
+    }
+  }
+  return {
+    path: path.resolve(__dirname, "dist"),
+    filename: `${libraryName}.js`,
+    library: {
+      name: libraryName,
+      type: 'umd',
+      umdNamedDefine: true
+    }
+  }
+}
 
 module.exports = {
   mode: "production",
-  entry: {
-    sm2: './src/sm2/index.js',
-    sm3: './src/sm3/index.js',
-    sm4: './src/sm4/index.js',
-  },
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: '[name].js',
-    library: '[name]',
-    libraryTarget: 'window',
-  },
+  entry: entryGenerator(CIPHER),
+  output: outputGenerator(CIPHER),
   module: {
     rules: [{
       test: /\.js$/,
